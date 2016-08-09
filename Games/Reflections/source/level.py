077444a243c2019b815966554a78dc5658b71d14
Fenrir190 <AzulStorm190@gmail.com>
2016-08-09 21:27:01 +0000

    Completed navigation method

--- Games/Reflections/source/level.py
+++ Games/Reflections/source/level.py
@@ -5,12 +5,66 @@ LEVEL_Y_VALUE = 3
 
 directions = ["foward", "left", "backwards", "right"]
 
-def navigate( area_map, direction):
-    print("Till next time. Stay gold")
+
+def navigate( level_obj, direction):
+    """
+    Purpose: This method calculates the new location of the player
+    after a move.
+    Return: Returns the new location of the player
+    @param level_obj - an object of a level
+    @param  direction - an integer denoting the direction the player will move
+    """
+    # print("Current Location of Player %d" %(area_map.index("x")))
+    current_location = level_obj.area_map.index("x")
+    start_location = current_location
+    cross_border = False
+    proc_rand_event = False
+
+    if(direction == 0): # Moving Up
+        if(current_location == 0 or current_location == 1 or current_location == 2):
+            print(level_obj.border_reached_text)
+        else:
+            current_location = current_location - 3
+    elif(direction == 1): # Moving left
+        if(current_location == 0 or  current_location == 6):
+            print(level_obj.border_reached_text)
+        elif(current_location == 3):
+            user_choice = int(input(level_obj.border_cross_text))
+
+            if(user_choice == 1):
+                cross_border = True
+        else:
+            current_location = current_location - 1
+    elif(direction == 2): # Moving down
+        if(current_location == 6 or current_location == 7 or current_location == 8):
+            print(level_obj.border_reached_text)
+        else:
+            current_location = current_location + 3
+    else: # Moving right
+        if(current_location == 2  or current_location == 8):
+            print(level_obj.border_reached_text)
+        elif(current_location == 5):
+            user_choice = int(input(level_obj.border_cross_text))
+
+            if(user_choice == 1):
+                cross_border = True
+        else:
+            current_location = current_location + 1
+
+        if(level_obj.area_map[current_location] == "r"):
+            proc_rand_event = True
+
+    # Update Map
+    level_obj.area_map[start_location] = ""
+    level_obj.area_map[current_location] = "x"
+
+    return [proc_rand_event, cross_border]
 
 class Forest:
     area_map = ["r","","","","x","r","r","","",]
     level_name = "forest"
+    border_reached_text = "You look onto the dense barrier of trees and turn back."
+    border_cross_text = "You look on to see an unfamiliar area. Enter?\n1 = yes\n2 = no\n"
     start_text = "The light filtering through the leaves of the tall trees\n" \
     + "creates a subtle shimmering effect around you.\n" \
     + "The lush green grass is spotted with patches of soft soil.\n" \
@@ -29,7 +83,7 @@ class Forest:
             print("Choosing to go the direction of the most interesting sound,\n" \
             + "you decide to go %s" %(directions[direction]))
 
-            navigate(self.area_map, direction)
+            map_state = navigate(self, direction)
 
         elif(user_choice == "2"):
             direction = int(input("Which direction will you go?\n1=up\n2=left\n" \
@@ -37,7 +91,7 @@ class Forest:
 
             print("You decide to go %s" %(directions[direction-1]))
 
-            navigate(self.area_map, direction)
+            map_state = navigate(self.area_map, direction)
 
         else:
             print("Where are you going?")
--- Games/Reflections/test/reflect_test.py
+++ Games/Reflections/test/reflect_test.py
@@ -1,4 +1,35 @@
-import unittest
+import sys
+sys.path.append('/home/fenrir/Coding/Games/Reflections')
 
-class ReflectTest:
-    
+import unittest
+import source.level
+
+class ReflectTest(unittest.TestCase):
+    def testNavigation(self):
+        forest_obj = source.level.Forest()
+
+        self.assertEqual(forest_obj.area_map[4], "x", "Invalid start location")
+
+        # Move Up
+        source.level.navigate(forest_obj, 0)
+        self.assertEqual(forest_obj.area_map[1], "x", "Player moved to the wrong space")
+
+        # Move down
+        source.level.navigate(forest_obj, 2)
+        self.assertEqual(forest_obj.area_map[4], "x", "Player moved to the wrong space")
+
+        # Move left
+        source.level.navigate(forest_obj, 1)
+        self.assertEqual(forest_obj.area_map[3], "x", "Player moved to the wrong space")
+
+        # Move right
+        source.level.navigate(forest_obj, 3)
+        self.assertEqual(forest_obj.area_map[4], "x", "Player moved to the wrong space")
+
+        # Move test borders
+        source.level.navigate(forest_obj, 3)
+        return_obj = source.level.navigate(forest_obj, 3)
+        self.assertTrue(return_obj[1], "Border Not Crossed")
+
+obj = ReflectTest()
+obj.testNavigation()
