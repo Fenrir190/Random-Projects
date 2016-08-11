12457188246ec9e787afcc40006ea2fbb823aec2
Fenrir190 <AzulStorm190@gmail.com>
2016-08-11 20:08:50 +0000

    work on navigation and level progression

--- Games/Reflections/source/level.py
+++ Games/Reflections/source/level.py
@@ -5,7 +5,6 @@ LEVEL_Y_VALUE = 3
 
 directions = ["foward", "left", "backwards", "right"]
 
-
 def navigate( level_obj, direction):
     """
     Purpose: This method calculates the new location of the player
@@ -18,6 +17,7 @@ def navigate( level_obj, direction):
     current_location = level_obj.area_map.index("x")
     start_location = current_location
     cross_border = False
+    next_level = -1
     proc_rand_event = False
 
     if(direction == 0): # Moving Up
@@ -32,7 +32,7 @@ def navigate( level_obj, direction):
             user_choice = int(input(level_obj.border_cross_text))
 
             if(user_choice == 1):
-                cross_border = True
+                next_level = 0
         else:
             current_location = current_location - 1
     elif(direction == 2): # Moving down
@@ -47,7 +47,7 @@ def navigate( level_obj, direction):
             user_choice = int(input(level_obj.border_cross_text))
 
             if(user_choice == 1):
-                cross_border = True
+                next_level = 1
         else:
             current_location = current_location + 1
 
@@ -58,10 +58,10 @@ def navigate( level_obj, direction):
     level_obj.area_map[start_location] = ""
     level_obj.area_map[current_location] = "x"
 
-    return [proc_rand_event, cross_border]
+    return [proc_rand_event, next_level]
 
 class Forest:
-    area_map = ["r","","","","x","r","r","","",]
+    area_map = []
     level_name = "forest"
     border_reached_text = "You look onto the dense barrier of trees and turn back."
     border_cross_text = "You look on to see an unfamiliar area. Enter?\n1 = yes\n2 = no\n"
@@ -73,8 +73,22 @@ class Forest:
     + "you decide to explore but are not sure on how.\n" \
     + "Either leave it to fate(1) or pick direction(2).\n"
 
+    def __init__(self, prev_level):
+        if (prev_level != None):
+            if(prev_level.level_name == "cave"):
+                self.area_map = ["r","","r","","","x","r","","",]
+
+            elif(prev_level.level_name == "valley"):
+                self.area_map = ["r","","r","x","","","r","","",]
+        else:
+            self.area_map = ["r","","","","x","r","r","","",]
+
+
     def choose_direction(self):
+        proceed_to_new_level = False
+        next_level = -1
         direction = 0
+
         user_choice = input("1 = Leave it to fate\n2 = Decide for yourself\n")
 
         if(user_choice == "1"):
@@ -91,13 +105,72 @@ class Forest:
 
             print("You decide to go %s" %(directions[direction-1]))
 
-            map_state = navigate(self.area_map, direction)
+            map_state = navigate(self, direction)
 
         else:
             print("Where are you going?")
 
+        if(map_state[1] != -1):
+            proceed_to_new_level = True
+            next_level = map_state[1]
+
+        return [proceed_to_new_level, next_level]
+
 class Cave:
+    area_map = []
     level_name = "cave"
+    border_reached_text = "You've reached the of the path. With nothing to see here you" \
+    + "\nturn back."
+    border_cross_text = "You peer out of the cave to see an unfamiliar area. Enter?\n1 = " \
+    + "yes\n2 = no\n"
+    start_text = "You hear deafening silence in the dimly lit room you find yourself in.\n" \
+    + "You examine the craggy feel of the walls around and dirt beneath your feet\n" \
+    + "You appear to be inside a cave. The silence is occassionally broken by some far\n" \
+    + " off sound. You don't feel anything hostile in the area. In fact, it's rather peaceful.\n" \
+    + "Your exploration of the cave commences. Not sure how to proceed you decide to\n"\
+    + "Either leave it to fate(1) or pick direction(2).\n"
+
+    def __init__(self, prev_level):
+        if(prev_level != None):
+            if(prev_level.level_name == "forest"):
+                self.area_map = ["r","","r","","","x","r","","",]
+
+            elif(prev_level.level_name == "valley"):
+                self.area_map = ["r","","r","","","","r","","x",]
+        else:
+            self.area_map = ["r","","","","x","r","r","","",]
+
+    def choose_direction(self):
+        proceed_to_new_level == False
+        next_level = -1
+        direction = 0
+
+        user_choice = input("1 = Leave it to fate\n2 = Decide for yourself\n")
+
+        if(user_choice == "1"):
+            direction = random.randint(0,3)
+
+            print("Choosing to go the direction of the most interesting sound,\n" \
+            + "you decide to go %s" %(directions[direction]))
+
+            map_state = navigate(self, direction)
+
+        elif(user_choice == "2"):
+            direction = int(input("Which direction will you go?\n1=up\n2=left\n" \
+            + "3=down\n4=right\n"))
+
+            print("You decide to go %s" %(directions[direction-1]))
+
+            map_state = navigate(self, direction)
+
+        else:
+            print("Where are you going?")
+
+        if(map_state[1] != -1):
+            proceed_to_new_level = True
+            next_level = map_state[1]
+
+        return [proceed_to_new_level, next_level]
 
 class Valley:
     level_name = "valley"
--- Games/Reflections/source/reflections.py
+++ Games/Reflections/source/reflections.py
@@ -1,11 +1,35 @@
 # Purpose: This file contains all tools needed for user interaction.
 
 import level
+proceed_next_level = False
 
 def begin_adventure( ):
-    first_level = level.Forest()
+    first_level = level.Forest(None)
     print("You awaken to find yourself in an unfamiliar %s." %first_level.level_name)
     print(first_level.start_text)
-    first_level.choose_direction()
+
+    while(proceed_next_level == False):
+        level_progression = first_level.choose_direction()
+
+        if(level_progression[0] == True):
+            proceed_next_level == True
+
+    continue_adventure(level_progression[1])
+
+def continue_adventure(new_level):
+    current_level = None
+
+    if(new_level == 0):
+        current_level = level.Valley()
+    elif(new_level == 1):
+        current_level = level.Cave()
+
+    while(proceed_next_level == False):
+        level_progression = current_level.choose_direction()
+
+        if(level_progression[0] == True):
+            proceed_next_level == True
+
+    continue_adventure(level_progression[1])
 
 begin_adventure()
